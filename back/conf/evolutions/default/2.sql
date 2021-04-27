ab7bc8450bea517915bfb7070c516b9d0af1be7e
mirkovicjul <mirkovicjul@gmail.com>
2021-04-27 15:21:59 +0200

    Change evolutions

--- back/conf/evolutions/default/2.sql
+++ back/conf/evolutions/default/2.sql
@@ -1,12 +1,12 @@
 -- !Ups
 
 CREATE TABLE measurements (
-	id INT PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	measurement VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE measurement_converter (
-	id INT PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	from_measurement INT REFERENCES measurements(id),
 	to_measurement INT REFERENCES measurements(id),
 	factor DECIMAL
