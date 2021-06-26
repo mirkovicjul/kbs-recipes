8589759fd72dd59ff3408abf93a16dd1ae3c18ee
Miloš Tepić <tepcmils@gmail.com>
2021-06-26 22:39:26 +0200

    Use JWT token in recommendation endpoint

--- back/app/controllers/RecommendationController.scala
+++ back/app/controllers/RecommendationController.scala
@@ -1,55 +1,57 @@
 package controllers
 
+import auth.AuthUtils
 import com.typesafe.scalalogging.LazyLogging
 import controllers.RecommendationController._
 import drools.recommendation.Recommendation
-import play.api.libs.json.{JsObject, JsPath, JsValue, Json, Reads, Writes}
-import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents, _}
+import play.api.libs.json.{JsValue, Json, Writes}
+import play.api.mvc._
 import services.RecommendationService
 
 import javax.inject._
 import scala.concurrent.ExecutionContext
 import scala.jdk.CollectionConverters.CollectionHasAsScala
 
-case class RecommendationRequest(userId: Long)
-
 class RecommendationController @Inject()(
     val controllerComponents: ControllerComponents,
     recommendationService: RecommendationService
 )(implicit ec: ExecutionContext)
-    extends BaseController with LazyLogging {
+    extends BaseController
+    with LazyLogging {
 
   def regular(): Action[AnyContent] = Action {
     implicit req: Request[AnyContent] =>
-      val requestParams: RecommendationRequest = req.body.asJson.get.as[RecommendationRequest]
-      logger.info(s"Recommending recipes for user ${requestParams.userId}...")
-
-      val recommendations: Seq[Recommendation] = recommendationService.recommendOnHomepage(requestParams.userId)
-
-      logger.info(s"Recommendation finished for user ${requestParams.userId}, recommended recipes ${recommendations.size}.")
-
-      Ok(Json.toJson(recommendations))
+      AuthUtils.extractUserId(req) match {
+        case Some(userId) =>
+          logger.info(s"Recommending recipes for user ${userId}...")
+          val recommendations: Seq[Recommendation] =
+            recommendationService.recommendOnHomepage(userId)
+          logger.info(
+            s"Recommendation finished for user ${userId}, recommended recipes ${recommendations.size}.")
+          Ok(Json.toJson(recommendations))
+        case None =>
+          BadRequest
+      }
   }
 }
 
 object RecommendationController {
 
-  implicit val recommendRequest: Reads[RecommendationRequest] =
-    (JsPath \ "user_id").read[Long].map(RecommendationRequest)
+  implicit val recommendation: Writes[Recommendation] =
+    new Writes[Recommendation] {
+      override def writes(o: Recommendation): JsValue =
+        Json.obj(
+          "recipe_id" -> o.getRecipeId.toString,
+          "hit"       -> o.getHit,
+          "messages" -> Json.arr(
+            o.getMessages.asScala.toSeq.map(Json.toJson[String]))
+        )
+    }
 
-  implicit val recommendation: Writes[Recommendation] = new Writes[Recommendation] {
-    override def writes(o: Recommendation): JsValue =
-      Json.obj(
-        "recipe_id" -> o.getRecipeId.toString,
-        "hit" -> o.getHit,
-        "messages" -> Json.arr(o.getMessages.asScala.toSeq.map(Json.toJson[String]))
-      )
-  }
-
-  implicit val recommendResponse: Writes[Seq[Recommendation]] = new Writes[Seq[Recommendation]] {
-    override def writes(o: Seq[Recommendation]): JsValue =
-      Json.obj("result" -> Json.arr(o.map(Json.toJson[Recommendation])))
-  }
-
+  implicit val recommendResponse: Writes[Seq[Recommendation]] =
+    new Writes[Seq[Recommendation]] {
+      override def writes(o: Seq[Recommendation]): JsValue =
+        Json.obj("result" -> Json.arr(o.map(Json.toJson[Recommendation])))
+    }
 
 }
