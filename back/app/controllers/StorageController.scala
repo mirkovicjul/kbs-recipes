c7885ec696b7db3b30e197b380417352e5b258cf
Miloš Tepić <tepcmils@gmail.com>
2021-06-28 00:31:13 +0200

    Add endpoint for getting ingredients from storage

--- back/app/controllers/StorageController.scala
+++ back/app/controllers/StorageController.scala
@@ -1,7 +1,8 @@
 package controllers
 
-import auth.UserAction
+import auth.{AuthUtils, UserAction}
 import com.typesafe.scalalogging.LazyLogging
+import database.IngredientStorageRepo
 import models.{IngredientStorage, RecipeStorage}
 import pdi.jwt.{JwtAlgorithm, JwtJson}
 import play.api.http.HeaderNames
@@ -9,28 +10,35 @@ import play.api.mvc._
 import play.libs.Json
 import services.{IngredientStorageService, RecipeStorageService, UserService}
 
+import java.util
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 import scala.jdk.CollectionConverters.SeqHasAsJava
 
 class StorageController @Inject()(
-                                   val controllerComponents: ControllerComponents,
-                                   userAction: UserAction,
-                                   ingredientStorageService: IngredientStorageService,
-                                   recipeStorageService: RecipeStorageService
-                                 )(implicit ec: ExecutionContext)
-  extends BaseController with LazyLogging {
+    val controllerComponents: ControllerComponents,
+    userAction: UserAction,
+    ingredientStorageService: IngredientStorageService,
+    recipeStorageService: RecipeStorageService,
+    ingredientStorageRepo: IngredientStorageRepo
+)(implicit ec: ExecutionContext)
+    extends BaseController
+    with LazyLogging {
 
   def getIngredientStorage(): Action[AnyContent] = userAction.async { request =>
-
     val token: Option[String] = request.headers.get(HeaderNames.AUTHORIZATION)
 
-    token.flatMap(t => JwtJson.decodeJson(t, "secretKey", Seq(JwtAlgorithm.HS256)).toOption) match {
+    token.flatMap(
+      t =>
+        JwtJson
+          .decodeJson(t, "secretKey", Seq(JwtAlgorithm.HS256))
+          .toOption) match {
       case Some(value) =>
         logger.debug("Before user ID")
         val userId = (value \ "userId").as[Long]
         logger.debug("After user ID")
-        val res: java.util.List[IngredientStorage] = ingredientStorageService.getIngredientStorage(userId).asJava
+        val res: java.util.List[IngredientStorage] =
+          ingredientStorageService.getIngredientStorage(userId).asJava
         //ingredientStorageService.getIngredientStorage(userId).foreach(ing => println(ing.getMeasurement.getMeasurement))
         Future(Ok(Json.toJson(res).toString))
 
@@ -40,16 +48,33 @@ class StorageController @Inject()(
   }
 
   def getRecipeStorage(): Action[AnyContent] = userAction.async { request =>
-
     val token: Option[String] = request.headers.get(HeaderNames.AUTHORIZATION)
 
-    token.flatMap(t => JwtJson.decodeJson(t, "secretKey", Seq(JwtAlgorithm.HS256)).toOption) match {
+    token.flatMap(
+      t =>
+        JwtJson
+          .decodeJson(t, "secretKey", Seq(JwtAlgorithm.HS256))
+          .toOption) match {
       case Some(value) =>
         val userId = (value \ "userId").as[Long]
-        val res: java.util.List[RecipeStorage] = recipeStorageService.getRecipeStorage(userId).asJava
+        val res: java.util.List[RecipeStorage] =
+          recipeStorageService.getRecipeStorage(userId).asJava
         Future(Ok(Json.toJson(res).toString))
       case None => Future(Forbidden)
     }
 
   }
+
+  def getIngredientsForRecipe(recipeId: Long) = Action.apply { request =>
+    AuthUtils.extractUserId(request) match {
+      case Some(userId) =>
+        logger.info(s"Getting ingredients for user $userId and recipe $recipeId.")
+        val recipeIngredients: util.List[IngredientStorage] =
+          ingredientStorageRepo.getIngredientsForRecipe(recipeId, userId)
+        Ok(Json.toJson(recipeIngredients).toString)
+      case None =>
+        BadRequest
+    }
+  }
+
 }
